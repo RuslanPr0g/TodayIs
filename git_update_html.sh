0a5043ceca2bbbca0d13297cdd685c079de4e0a9
Ruslan Rudenko <theruslan.prog@gmail.com>
2025-07-03 14:43:08 +0200

    fixes

--- git_update_html.sh
+++ git_update_html.sh
@@ -26,6 +26,10 @@ log "Successfully wrote today's date to index.html."
 log "Waiting for 20 seconds before proceeding with git add."
 sleep 20
 
+git restore .
+
+git clean -f
+
 git pull --rebase origin master || { log "ERROR: Git pull failed."; exit 1; }
 
 git add . || { log "ERROR: Failed to add files to Git."; exit 1; }
